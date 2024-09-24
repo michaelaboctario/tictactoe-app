da908c352c2e598558d767cf006495d69959cb9a
Michaela Bucatariu <mbucatariu24@gmail.com>
2024-09-24 11:29:18 +0200

    change some styles

--- src/App.js
+++ src/App.js
@@ -30,24 +30,25 @@ function Board({ xIsNext, squares, onPlay }) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
+  const arrindex = [];
+  for(let i=0; i<9; i++)
+    arrindex.push(i);
+  console.log(arrindex);
+
+  const boardsquares = arrindex.map((index) => {
+    const value=squares[index];
+    return (
+        <Square key={index} value={value} onSquareClick={() => handleClick(index)} />   
+    )
+  })
+ // console.log(boardsquares);
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      <div className="board"> 
+        {boardsquares}
+      </div>       
     </>
   );
 }
@@ -81,6 +82,8 @@ export default function Game() {
       </li>
     );
   });
+
+  console.log(moves);
 
   return (
     <div className="game">
